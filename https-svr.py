12b90d4be161c42234f261fb9bd65853e9a0811e
root <root@vmSmartBoxIot-2.rpyce2w4vk3ubfarhbu5kthuve.cx.internal.cloudapp.net>
2018-01-16 14:04:21 -0500

    init version

---
+++ https-svr.py
@@ -0,0 +1,41 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import ssl
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+	content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        
+        self._set_headers()
+        self.wfile.write("<html><body><h1>"+ post_data  +"</h1></body></html>")
+	print("payload" +  post_data) 
+
+        
+def run(server_class=HTTPServer, handler_class=S, port=443):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
